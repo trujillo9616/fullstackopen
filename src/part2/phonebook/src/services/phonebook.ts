3501ce61ebb8aeaa36484465ba30e66d692b3a7a
Adrian Trujillo <adrian.td96@gmail.com>
2023-01-06 15:05:12 -0500

    perf: :recycle: updated baseUrl
    Updated baseUrl for the services calls. The new url is relative because the backend will serve the production build, when using the dev environment a proxy will enter.

--- src/part2/phonebook/src/services/phonebook.ts
+++ src/part2/phonebook/src/services/phonebook.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Person } from '../types';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = '/api/persons';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
