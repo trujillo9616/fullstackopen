c13e2766496c6d91805c05124b07bd287ba16304
Adrian Trujillo <adrian.td96@gmail.com>
2023-01-06 15:03:03 -0500

    feat: :heavy_plus_sign: added dotenv
    Added dotenv dependency package to read env files and acces env variables.

--- src/part3/phonebook-server/src/index.ts
+++ src/part3/phonebook-server/src/index.ts
@@ -1,8 +1,11 @@
-import express from 'express';
-import morgan from 'morgan';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 import { generateId } from './utils';
 import { Person } from './types';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -31,28 +34,29 @@ let db: Person[] = [
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('build'));
 app.use(cors());
-morgan.token('body', (req: express.Request) => JSON.stringify(req.body));
+morgan.token('body', (req: Request) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 // Get Routes
-app.get('/api', (_req: express.Request, res: express.Response) => {
+app.get('/api', (_req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.get('/api/info', (_req: express.Request, res: express.Response) => {
+app.get('/api/info', (_req: Request, res: Response) => {
   const date = new Date();
   const info = `Phonebook has info for ${db.length} people\n
   ${date}`;
   res.send(info);
 })
 
-app.get('/api/persons', (_req: express.Request, res: express.Response) => {
+app.get('/api/persons', (_req: Request, res: Response) => {
   res.json(db);
 });
 
-app.get('/api/persons/:id', (req: express.Request, res: express.Response) => {
+app.get('/api/persons/:id', (req: Request, res: Response) => {
   const id = Number(req.params.id);
   const person = db.find(p => p.id === id);
 
@@ -64,14 +68,14 @@ app.get('/api/persons/:id', (req: express.Request, res: express.Response) => {
 });
 
 // Delete Routes
-app.delete('/api/persons/:id', (req: express.Request, res: express.Response) => {
+app.delete('/api/persons/:id', (req: Request, res: Response) => {
   const id = Number(req.params.id);
   db = db.filter(p => p.id !== id);
   res.status(204).end();
 });
 
 // Post Routes
-app.post('/api/persons', (req: express.Request, res: express.Response) => {
+app.post('/api/persons', (req: Request, res: Response) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
