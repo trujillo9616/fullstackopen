9ccc1b815c6a5df793b6e332f9e29ed3b4e8a93c
Adrian Trujillo <adrian.td96@gmail.com>
2023-01-06 18:30:50 -0500

    feat: :card_file_box: added mongodb for data management
    Added a connection to a MongoDB Atlas Cluster as a DB for managing the data in the requests and persist the information.

--- src/part3/phonebook-server/src/index.ts
+++ src/part3/phonebook-server/src/index.ts
@@ -1,36 +1,34 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import { generateId } from './utils';
-import { Person } from './types';
+import mongoose from 'mongoose';
+import Person from './models/person';
 
 dotenv.config();
-
 const PORT = process.env.PORT || 3001;
 
-let db: Person[] = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
+const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
   }
-];
+
+  return next(error);
+};
+
+// Connect to MongoDB
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/phonebook';
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB:', error.message);
+  });
 
 const app = express();
 app.use(express.json());
@@ -40,42 +38,48 @@ app.use(cors());
 morgan.token('body', (req: Request) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-// Get Routes
+
+// Routes
 app.get('/api', (_req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.get('/api/info', (_req: Request, res: Response) => {
+app.get('/api/info', async (_req: Request, res: Response) => {
+  const db = await Person.find({});
   const date = new Date();
   const info = `Phonebook has info for ${db.length} people\n
   ${date}`;
   res.send(info);
 })
 
-app.get('/api/persons', (_req: Request, res: Response) => {
-  res.json(db);
+app.get('/api/persons', async (_req: Request, res: Response) => {
+  const persons = await Person.find({});
+  res.json(persons);
 });
 
-app.get('/api/persons/:id', (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const person = db.find(p => p.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
+app.get('/api/persons/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
-// Delete Routes
-app.delete('/api/persons/:id', (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  db = db.filter(p => p.id !== id);
-  res.status(204).end();
+app.delete('/api/persons/:id', async (req: Request, res: Response) => {
+  try {
+    await Person.findByIdAndRemove(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).send({ error: 'malformatted id' });
+  }
 });
 
-// Post Routes
-app.post('/api/persons', (req: Request, res: Response) => {
+app.post('/api/persons', async (req: Request, res: Response) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -84,22 +88,40 @@ app.post('/api/persons', (req: Request, res: Response) => {
     });
   }
 
-  if (db.find(p => p.name === body.name)) {
+  const persons = await Person.find({});
+  if (persons.find(p => p.name === body.name)) {
     return res.status(400).json({
       error: 'name must be unique'
     });
   }
 
-  const person = {
-    id: generateId(db),
+  const person = new Person({
     name: body.name,
-    number: body.number
-  }
+    number: body.number,
+  });
 
-  db = db.concat(person);
+  const savedPerson = await person.save();
 
-  return res.json(person);
+  return res.json(savedPerson);
 });
+
+app.put('/api/persons/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(req.params.id, person, { new: true });
+    res.json(updatedPerson);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
