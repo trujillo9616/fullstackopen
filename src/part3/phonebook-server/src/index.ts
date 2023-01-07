97427596d8f8ce3ff93a355712659b3e2f56f921
Adrian Trujillo <adrian.td96@gmail.com>
2023-01-07 02:00:38 -0500

    feat: :sparkles: added error handling
    Added error handling on the post request. A new error handling middle ware was created.

--- src/part3/phonebook-server/src/index.ts
+++ src/part3/phonebook-server/src/index.ts
@@ -8,16 +8,6 @@ import Person from './models/person';
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
-const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
-  console.error(error.message);
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
-  }
-
-  return next(error);
-};
 
 // Connect to MongoDB
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/phonebook';
@@ -54,17 +44,51 @@ app.get('/api/info', async (_req: Request, res: Response) => {
 
 app.get('/api/persons', async (_req: Request, res: Response) => {
   const persons = await Person.find({});
-  res.json(persons);
+  res.json(persons.map(person => person.toJSON()));
 });
 
 app.get('/api/persons/:id', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const person = await Person.findById(req.params.id);
     if (person) {
-      res.json(person);
+      res.json(person.toJSON());
     } else {
       res.status(404).end();
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/api/persons', async (req: Request, res: Response, next: NextFunction) => {
+  const body = req.body;
+
+  const persons = await Person.find({});
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  try {
+    const savedPerson = await person.save();
+    return res.json(savedPerson);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.put('/api/persons/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const { name, number } = req.body;
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true, context: 'query' });
+    res.json(updatedPerson);
   } catch (error) {
     next(error);
   }
@@ -79,47 +103,22 @@ app.delete('/api/persons/:id', async (req: Request, res: Response) => {
   }
 });
 
-app.post('/api/persons', async (req: Request, res: Response) => {
-  const body = req.body;
+const unknownEndpoint = (_req: Request, res: Response) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: 'content missing'
-    });
+app.use(unknownEndpoint);
+
+const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
   }
 
-  const persons = await Person.find({});
-  if (persons.find(p => p.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    });
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
-
-  const savedPerson = await person.save();
-
-  return res.json(savedPerson);
-});
-
-app.put('/api/persons/:id', async (req: Request, res: Response, next: NextFunction) => {
-  const body = req.body;
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  try {
-    const updatedPerson = await Person.findByIdAndUpdate(req.params.id, person, { new: true });
-    res.json(updatedPerson);
-  } catch (error) {
-    next(error);
-  }
-});
+  return next(error);
+};
 
 app.use(errorHandler);
 
--- src/part3/phonebook-server/src/models/person.ts
+++ src/part3/phonebook-server/src/models/person.ts
@@ -1,8 +1,22 @@
 import mongoose from 'mongoose';
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: (v: string) => {
+        return /\d{2,3}-\d*/.test(v);
+      }
+    }
+  },
 });
 
 personSchema.set('toJSON', {
